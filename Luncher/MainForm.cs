05e16343d9e57ffee5c72f3f30fc4973d2f886d6
Igor Popov <dedepete@mail.ru>
2014-06-25 03:53:50 +0400

    Update MainForm.cs

--- Luncher/MainForm.cs
+++ Luncher/MainForm.cs
@@ -338,10 +338,6 @@ namespace Luncher
                 }
             }
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
         private void CheckApplicationUpdate(object sender, EventArgs e)
         {
             if (LoadConfiguration.Updaterupdateprogram == "True")
@@ -350,7 +346,6 @@ namespace Luncher
                 Invoke(mi1);
                 try
                 {
-<<<<<<< HEAD
                     var aver = new WebClient().DownloadString("http://file.ru-minecraft.ru/verlu.html");
                     if (aver == ProductVersion)
                     {
@@ -370,31 +365,6 @@ namespace Luncher
                             Text = @"Найдено обновление",
                             MessageText =
                                 "<html>Найдено обновление лаунчера: <b>" + aver + "</b>\nТекущая версия: <b>" +
-=======
-                    var request =
-                        (HttpWebRequest) WebRequest.Create("http://file.ru-minecraft.ru/verlu.html");
-                    var response = (HttpWebResponse) request.GetResponse();
-                    var sr = new StreamReader(response.GetResponseStream());
-                    var line = sr.ReadLine();
-                    if (line == ProductVersion)
-                    {
-                        var mi2 = new MethodInvoker(delegate
-                        {
-                            WriteLog("No update found.");
-                            CheckVersions();
-                        });
-                        Invoke(mi2);
-                    }
-                    if (line != ProductVersion)
-                    {
-                        var mi2 = new MethodInvoker(() => WriteLog("Update avaible: " + line));
-                        Invoke(mi2);
-                        var dr = new RadMessageBoxForm
-                        {
-                            Text = @"Найдено обновление",
-                            MessageText =
-                                "<html>Найдено обновление лаунчера: <b>" + line + "</b>\nТекущая версия: <b>" +
->>>>>>> origin/master
                                 ProductVersion +
                                 "</b>\n Хотите ли вы пройти на страницу загрузки данного обновления?\n\nP.S. В противном случае, это уведомление будет появляться при каждом запуске лаунчера >:3",
                             StartPosition = FormStartPosition.CenterScreen,
@@ -414,7 +384,6 @@ namespace Luncher
                             Invoke(mi);
                         }
                     }
-                    response.Close();
                 }
                 catch (Exception ex)
                 {
