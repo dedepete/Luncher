3bb04db3f508660e445c136820b13d3bd821a68a
Igor Popov <dedepete@mail.ru>
2014-07-03 22:17:05 +0400

    Updated to 1.2.6

--- Luncher/Properties/AssemblyInfo.cs
+++ Luncher/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.5.1596")]
-[assembly: AssemblyFileVersion("1.2.5.1596")]
+[assembly: AssemblyVersion("1.2.6.1612")]
+[assembly: AssemblyFileVersion("1.2.6.1612")]
 [assembly: NeutralResourcesLanguageAttribute("ru-RU")]
